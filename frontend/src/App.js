204734881df5a85d9e8f97001feb6bdc5c003859
Ke Li <likelike101101@gmail.com>
2022-08-10 23:27:13 -0400

    Add Homepage frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Footer from './components/Footer';
 import Header from './components/Header'
-import LoginScreen from './screens/LoginScreen'
+import PatientRegisterScreen from './screens/PatientRegisterScreen';
 import HomeScreen from "./screens/HomeScreen";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Container>
           <Routes>
             <Route path='' element={<HomeScreen />} exact />
-            <Route path='/login' element={<LoginScreen />} />
+            <Route path='/login' element={<PatientRegisterScreen />} />
           </Routes>
         </Container>
       </main>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,53 @@
 import React from 'react'
-import { Button, Container } from 'react-bootstrap'
+import { Container ,Row, Col} from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 function HomeScreen() {
   return (
     <Container>
-        <h3>HomeScreen</h3>
-        <LinkContainer to="/login">
-            <Button className="btn btn-outline-primary bg-secondary">login</Button>
-        </LinkContainer>
+        <Container className="card">
+          <Container className="card-body" style={{ "textAlign": "center" }}>
+          <h1
+            className="mt-3"
+            style={{ "fontSize": "60px", "fontFamily": "Poppins" , "color": "#2196f3"}}
+          >
+            I am a
+          </h1>
+          <Row className='mt-5'>
+            <Col>
+              <LinkContainer to="/patient/login" className="card" style={{"marginLeft":"60px", "backgroundColor":"#EBF4FF"}}>
+                <div>
+                  <img
+                  alt="" 
+                  src="/patient_icon.png"
+                  width="500"
+                  height="500">
+                    
+                  </img>
+                  <h4 style={{ "fontSize": "50px", "fontFamily": "Poppins" , "color": "#2196f3"}}>
+                    Patient
+                  </h4>
+                </div>
+              </LinkContainer>
+            </Col>
+            <Col>
+              <LinkContainer to="/physician/login" className="card" style={{"marginRight":"40px" , "backgroundColor":"#EBF4FF"}}>
+              <div>
+                  <img
+                  alt="" 
+                  src="/physician_icon.png"
+                  width="500"
+                  height="500">
+                  </img>
+                  <h4 style={{ "fontSize": "50px", "fontFamily": "Poppins" , "color": "#2196f3"}}>
+                    Physician
+                  </h4>
+                </div>
+              </LinkContainer>
+            </Col>
+          </Row>
+          </Container>
+
+        </Container>
     </Container>
   )
 }
